9858fbf4ae7a52ec37eb6d7c484eff75b8a61326
Baihaqi34 <121011402+Baihaqi34@users.noreply.github.com>
2024-01-23 20:01:14 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,30 +1,16 @@
 const mysql = require('mysql');
 
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'username',
-//   password: '',
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'username',
+  password: '',
 
-// });
+});
 
-// // Membuka koneksi ke database
-
-
+// Membuka koneksi ke database
 
 
-const connection = mysql.createPool({
-	  host: 'localhost',
-	  user: 'username',
-	  password: '',
-	  database:'ci_crud'
-	});
 
-
-if (connection) {
-	console.log('succes');
-}else{
-	console.log('failed');
-}
 
 
 module.exports = connection;
